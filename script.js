7a820976538788c8492cb6d1f6489135c5222614
Nastia Kanapelka <raspisnastia@gmail.com>
2023-01-22 16:18:27 +0300

    added animation h1 and .paino

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+gsap.from("h1", {x: -100, duration: 2, opacity: 0});
+gsap.from(".piano", {x: -100, duration: 2, opacity: 0, delay: 2});
+
 const keys = document.querySelectorAll(".key");
 
 keys.forEach((key) => {
